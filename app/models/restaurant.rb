0f547069360bf7db930fcc324c64dd18793c3af2
Adam Ringrose <whatsyerpoison1@gmail.com>
2021-08-18 15:21:47 +0100

    models finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,11 @@
 class Restaurant < ApplicationRecord
   CATEGORY = %w(chinese italian belgian japanese french)
 
-  has_many :reviews, :dependent => :delete_all
+  has_many :reviews, dependent: :destroy
 
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: (0..5) }
+  validates :rating, numericality: { only_integer: true }
+
 end
